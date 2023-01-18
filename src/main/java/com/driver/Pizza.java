9f59149826709fc8145e71bcc27698693686ff37
Sowndharya <sowndharyaktr@gmail.com>
2023-01-18 12:37:30 +0530

    first commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,29 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean addTakeaway;
+    private boolean addExtraCheese;
+    private boolean addExtraToppings;
+    private int paperBag;
+    private int extraCheese;
+    private int extraToppings;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+       this.addTakeaway=false;
+       this.addExtraCheese=false;
+       this.addExtraToppings=false;
+       this.paperBag=20;
+       this.extraCheese=80;
+       if(isVeg){
+           this.price=300;
+           this.extraToppings=70;
+       }
+       else{
+           this.price=400;
+           this.extraToppings=120;
+       }
+       this.bill="Base Price Of The Pizza: "+price+"\n";
     }
 
     public int getPrice(){
@@ -16,19 +35,38 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(addExtraCheese==false){
+            this.price=price+extraCheese;
+            addExtraCheese=true;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(addExtraToppings==false){
+            this.price=price+extraToppings;
+            addExtraToppings=true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+       if(addTakeaway==false){
+           this.price=price+paperBag;
+           addTakeaway=true;
+       }
     }
 
     public String getBill(){
-        // your code goes here
+       if(addTakeaway){
+           this.bill=this.bill+"Paperbag Added: "+paperBag+"\n";
+       }
+       if(addExtraCheese){
+           this.bill=this.bill+"Extra Cheese Added: "+extraCheese+"\n";
+       }
+       if(addExtraToppings){
+           this.bill=this.bill+"Extra Toppings Added: "+extraToppings+"\n";
+       }
+
+       this.bill=this.bill+"Total Price: "+this.price+"\n";
         return this.bill;
     }
 }
